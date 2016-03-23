28aa9fc91e3fc6a92acdeba54daa02372f1741f5
Boopathi Rajaa <me@boopathi.in>
2016-03-23 01:17:35 +0100

    Add warning for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+
+const minify = new webpack.optimize.UglifyJsPlugin({
+  comments: false,
+  compress: {
+    warnings: false,
+    conditionals: true,
+    unused: true,
+    comparisons: true,
+    sequences: true,
+    dead_code: true,
+    evaluate: true,
+    if_return: true,
+    join_vars: true,
+    pure_getters: true,
+    unsafe: true,
+    unsafe_comps: true,
+    screw_ie8: true,
+    properties: true
+  }
+});
+
 module.exports = function (opts) {
   return {
     entry: './src/index.js',
@@ -11,6 +32,9 @@ module.exports = function (opts) {
         {
           test: /\.js$/,
           loader: 'babel',
+          // not yet because some node_modules are ES6
+          // and UglifyJs doesn't support ES6 yet
+          // exclude: /node_modules/,
           query: {
             presets: ['es2015-webpack']
           }
@@ -18,25 +42,7 @@ module.exports = function (opts) {
       ]
     },
     plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        comments: false,
-        compress: {
-          warnings: false,
-          conditionals: true,
-          unused: true,
-          comparisons: true,
-          sequences: true,
-          dead_code: true,
-          evaluate: true,
-          if_return: true,
-          join_vars: true,
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-          screw_ie8: true,
-          properties: true
-        }
-      })
+      minify
     ]
   };
 }
